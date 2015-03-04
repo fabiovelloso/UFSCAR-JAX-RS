f11f307766b1d8c428fc4d044d789af065d2c493
Fabio Velloso <github@fabiovelloso.com.br>
2015-03-04 00:28:24 -0300

    CPFValidator

--- src/java/br/ufscar/validators/CPFValidator.java
+++ src/java/br/ufscar/validators/CPFValidator.java
@@ -1,23 +1,22 @@
 /**
- * Copyright 2015 Fabio Velloso 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Copyright 2015 Fabio Velloso
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package br.ufscar.validators;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -27,65 +26,77 @@ public class CPFValidator implements ConstraintValidator<CPF, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        String strCpf;
-        strCpf = value.trim();
-        if (strCpf.equals("")) {
-            return false;
+        String cpf;
+        boolean valido = true;
+        cpf = value.trim();
+
+        switch (cpf) {
+            case "":
+                valido = false;
+                break;
+            case "00000000000":
+                valido = false;
+                break;
+            case "11111111111":
+                valido = false;
+                break;
+            case "22222222222":
+                valido = false;
+                break;
+            case "33333333333":
+                valido = false;
+                break;
+            case "44444444444":
+                valido = false;
+                break;
+            case "55555555555":
+                valido = false;
+                break;
+            case "66666666666":
+                valido = false;
+                break;
+            case "77777777777":
+                valido = false;
+                break;
+            case "88888888888":
+                valido = false;
+                break;
+            case "99999999999":
+                valido = false;
+                break;
         }
 
-        int d1, d2;
-        int digito1, digito2, resto;
-        int digitoCPF;
-        String nDigResult;
+        if (valido != false) {
+            int primeiraSoma=0;
+            int segundaSoma=0;
+            int primeiroDigito;
+            int segundoDigito;
+            int resto;
+            int aux;
+            String digitosCalculados;
+            String digitoVerificadorCPF;
 
-        d1 = 0;
-        d2 = 0;
+            for (int i=1; i < cpf.length()-1; i++) {
+                aux = Integer.parseInt(cpf.substring(i-1, i));
+                primeiraSoma += (11-i) * aux;
+                segundaSoma += (12-i) * aux;
+            }
+            resto = (primeiraSoma % 11);
+            primeiroDigito = (resto < 2) ? 0 : (11-resto);
+            segundaSoma += 2 * primeiroDigito;
+            resto = (segundaSoma % 11);
+            segundoDigito = (resto < 2) ? 0 : (1 -resto);
 
-        for (int count = 1; count < strCpf.length() - 1; count++) {
-            digitoCPF = Integer.parseInt(strCpf.substring(count - 1, count));
-
-            //multiplique a ultima casa por 2 a seguinte por 3 a seguinte por 4 e assim por diante.  
-            d1 = d1 + (11 - count) * digitoCPF;
-
-            //para o segundo digito repita o procedimento incluindo o primeiro digito calculado no passo anterior.  
-            d2 = d2 + (12 - count) * digitoCPF;
+            digitoVerificadorCPF = cpf.substring(cpf.length()-2, cpf.length());
+            digitosCalculados = String.valueOf(primeiroDigito)+String.valueOf(segundoDigito);
+            valido = digitoVerificadorCPF.equals(digitosCalculados);
         }
-
-        //Primeiro resto da divisão por 11.  
-        resto = (d1 % 11);
-
-        //Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.  
-        if (resto < 2) {
-            digito1 = 0;
-        } else {
-            digito1 = 11 - resto;
-        }
-
-        d2 += 2 * digito1;
-
-        //Segundo resto da divisão por 11.  
-        resto = (d2 % 11);
-
-        //Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.  
-        if (resto < 2) {
-            digito2 = 0;
-        } else {
-            digito2 = 11 - resto;
-        }
-
-        //Digito verificador do CPF que está sendo validado.  
-        String nDigVerific = strCpf.substring(strCpf.length() - 2, strCpf.length());
-
-        //Concatenando o primeiro resto com o segundo.  
-        nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
-
-        //comparar o digito verificador do cpf com o primeiro resto + o segundo resto.  
-        return nDigVerific.equals(nDigResult);
+        return valido;
     }
 
     public static void main(String[] args) {
-        CPFValidator valida = new CPFValidator();
-        System.out.println(valida.isValid("16492364848", null));
+        CPFValidator cpf = new CPFValidator();
+        //  System.out.println(cpf.isValid("00000000000", null));
     }
 
     @Override
